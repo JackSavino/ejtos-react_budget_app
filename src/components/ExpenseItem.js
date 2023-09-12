3fe5802ce80f447819bf1afc33260d9e794a04cf
JackSavino <jack_savino@yahoo.com>
2023-09-12 17:13:15 -0400

    Adding final changes to *.js files for budget allocation project

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { IoIosAddCircle, IoIosRemoveCircle } from 'react-icons/io';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -17,19 +18,41 @@ const ExpenseItem = (props) => {
             name: name,
             cost: 10,
         };
-
+    
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
+    }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+    
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
     }
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>
+            <button onClick={(event) => increaseAllocation(props.name)}
+            style={{ backgroundColor: 'white', border: 'none' }}>
+                <IoIosAddCircle size={30} style={{ color: 'green' }} />
+            </button>
+        </td>
+        <td>
+            <button onClick={(event) => decreaseAllocation(props.name)}
+            style={{ backgroundColor: 'white', border: 'none' }}>
+                <IoIosRemoveCircle size={30} style={{ color: 'maroon' }} />
+            </button>
+        </td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
