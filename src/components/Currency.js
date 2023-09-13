df27abae09daf4c60d67662feda5e1b5ae761637
JackSavino <jack_savino@yahoo.com>
2023-09-13 01:28:35 -0400

    Adding changes to Currency.js code for new dropdown design

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -10,22 +10,57 @@ const Currency = () => {
         changeCurrency(event.target.value);
     };
 
-    return (
-        <div className='alert alert-secondary'>
-            Currency:
-            <select
-                name="Currency"
-                id="Currency"
-                value={currency} // Set the selected value based on the context
-                onChange={handleCurrencyChange}
-            >
-                <option value="$">$ Dollar</option>
-                <option value="£">£ Pound</option>
-                <option value="₹">₹ Rupee</option>
-                <option value="€">€ Euro</option>
-            </select>
-        </div>
+const buttonStyle = {
+    backgroundColor: 'mistyrose',
+    color: 'maroon',
+    borderColor: 'maroon'
+    };
+
+return (
+    <div className="dropdown">
+    <select
+        className="btn btn-secondary dropdown-toggle"
+        value={currency}
+        style={buttonStyle}
+        onChange={handleCurrencyChange}
+        >
+        {currency && (
+          <option value={currency}>Currency ({currency})</option>
+        )} 
+        <option value="$">$ Dollar</option>
+        <option value="£">£ Pound</option>
+        <option value="₹">₹ Rupee</option>
+        <option value="€">€ Euro</option>
+        </select>
+    </div>
     );
 };
 
 export default Currency;
+
+// const Currency = () => {
+//     const { currency, changeCurrency } = useContext(AppContext);
+
+//     const handleCurrencyChange = (event) => {
+//         changeCurrency(event.target.value);
+//     };
+
+//     return (
+//         <div className='alert alert-secondary'>
+//             Currency:
+//             <select
+//                 name="Currency"
+//                 id="Currency"
+//                 value={currency} // Set the selected value based on the context
+//                 onChange={handleCurrencyChange}
+//             >
+//                 <option value="$">$ Dollar</option>
+//                 <option value="£">£ Pound</option>
+//                 <option value="₹">₹ Rupee</option>
+//                 <option value="€">€ Euro</option>
+//             </select>
+//         </div>
+//     );
+// };
+
+// export default Currency;
